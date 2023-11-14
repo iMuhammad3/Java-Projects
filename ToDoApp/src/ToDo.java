3619dad4495e334d7ab090cd6b72c1b6a47b6ebf
Muhammad <auwalmuhd365@gmail.com>
2023-11-14 14:03:41 +0100

    add fields, getters and setters for todo class

--- ToDoApp/src/ToDo.java
+++ ToDoApp/src/ToDo.java
@@ -1,3 +1,53 @@
+import java.util.Date;
+
 public class ToDo {
 
+    private int id;
+    private String description;
+    private boolean completed;
+    private Date dateCreated;
+    private Date lastModified;
+    public ToDo(String description){
+        this.description = description;
+    }
+
+    public Date getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Date lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
