c3349889e0758fed16c58da67eb21985b38e7b0b
Muhammad <auwalmuhd365@gmail.com>
2024-04-18 22:00:46 +0100

    display todos in table

--- ToDoWebApp/frontend/app.js
+++ ToDoWebApp/frontend/app.js
@@ -3,9 +3,13 @@ fetch("http://localhost:8080/todos")
     .then(res => res.json())
     .then(data => {
         data.forEach(todo => {
-            const li = document.createElement('li');
-            li.textContent = todo.todo;
-            todoList.appendChild(li);
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${todo.id}</td>
+                <td>${todo.todo}</td>
+                <td>${todo.completed}</td>
+            `
+            todoList.appendChild(row);
         });
     })
     .catch(err => console.error(err));
